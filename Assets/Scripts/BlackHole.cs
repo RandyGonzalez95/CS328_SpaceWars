355609c08a9b33c8d43f223a3e5cd4755be63fd6
Randy Gonzalez <randygonzalez@nevada.unr.edu>
2017-02-25 22:45:10 -0800

    Added Gravitational Pull

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    public float gravitationalForce = 5f;
+    private Vector3 distanceFromCenter;
+
+    GameObject gameobject;
+	// Use this for initialization
+	void Start ()
+    {
+        gameobject = GameObject.FindGameObjectWithTag("gameobject");
+        distanceFromCenter = Vector3.zero;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if(gameobject != null)
+        {
+            distanceFromCenter = (transform.position - gameobject.transform.position).normalized;
+            gameobject.GetComponent<Rigidbody2D>().AddForce(distanceFromCenter * gravitationalForce);
+        }      
+       
+    }
+}
