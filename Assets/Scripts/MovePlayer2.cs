5852917cf71f166a6607cf198c7023e8d7b9bae4
Randy Gonzalez <randygonzalez@nevada.unr.edu>
2017-02-26 21:34:13 -0800

    Added player 2

--- Assets/Scripts/MovePlayer2.cs
+++ Assets/Scripts/MovePlayer2.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer2 : MonoBehaviour {
+public class MovePlayer2 : MonoBehaviour
+{
+    public float speed;
+    public float thrustForce;
+    Rigidbody2D player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject bullet;
+    Transform firePosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        firePosition = transform.FindChild("FirePoint");
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            Instantiate(bullet, firePosition.transform.position, firePosition.transform.rotation);
+            //GetComponent<AudioSource>().Play();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Character Goes UP
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // move char up
+            player.AddForce(transform.up * thrustForce);
+        }
+        // Character goes left
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.angularVelocity = speed;
+        }
+        // Character Goes Down
+        else if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        // Character Goes Right
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.angularVelocity = -speed;
+        }
+        else
+        {
+            player.angularVelocity = 0f;
+        }
+    }
 }
