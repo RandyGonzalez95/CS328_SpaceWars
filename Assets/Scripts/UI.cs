15aa15e5469647d109cfdc486d60f7ed3a8f20ba
Randy Gonzalez <randygonzalez@nevada.unr.edu>
2017-02-28 00:38:30 -0800

    Added Pause/Resume

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public GameObject resumeButton;
+
+
+
+    public void OnPause()
+    {
+
+        if(resumeButton != null && pauseButton != null)
+        {
+            Time.timeScale = 0;
+            resumeButton.SetActive(true);
+            pauseButton.SetActive(false);
+        }
+
+    }
+
+    public void OnResume()
+    {
+
+        if (resumeButton != null && pauseButton != null)
+        {
+            Time.timeScale = 1;
+            resumeButton.SetActive(false);
+            pauseButton.SetActive(true);
+        }
+
+    }
+}
